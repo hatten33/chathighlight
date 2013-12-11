65eb43e44d27287e573ad1dbaa2d53d8e5882aa0
hatten33 <hatten33@gmail.com>
2013-12-10 19:22:05 -0800

    cleaned up code

--- src/net/aerenserve/chathighlight/ChatHighlight.java
+++ src/net/aerenserve/chathighlight/ChatHighlight.java
@@ -2,18 +2,31 @@ package net.aerenserve.chathighlight;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ChatHighlight extends JavaPlugin
-{
+/**
+ * ChatHighlight v1.2
+ * 
+ * You may use this code according to these conditions:
+ *
+ * 1. No warranty is given or implied.
+ * 2. All damage is your own responsibility.
+ * 3. You provide credit publicly to the original source should you release the plugin.
+ * 
+ * @author hatten33
+ */
+
+
+public final class ChatHighlight extends JavaPlugin {
+	
 	Listeners l;
+	
 	@Override
-	public void onEnable()
-	{
+	public void onEnable() {
 		l = new Listeners(this);
 		getLogger().info("Chathighlight enabled");
 	}
+	
 	@Override
-	public void onDisable()
-	{
+	public void onDisable() {
 		getLogger().info("ChatHighlight disabled");
 	}
 }
--- src/net/aerenserve/chathighlight/Listeners.java
+++ src/net/aerenserve/chathighlight/Listeners.java
@@ -10,25 +10,19 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public final class Listeners implements Listener
-{
+public final class Listeners implements Listener {
 	ChatHighlight chathighlight;
-	public Listeners(JavaPlugin plugin)
-	{
+	public Listeners(JavaPlugin plugin) {
 		chathighlight = (ChatHighlight) plugin;
 		chathighlight.getServer().getPluginManager().registerEvents(this, plugin);
 	}
+	
 	@EventHandler
-	public void onChatEvent(AsyncPlayerChatEvent event)
-	{
+	public void onChatEvent(AsyncPlayerChatEvent event) {
 		String message = event.getMessage();
-		//Player player = event.getPlayer();
 		Player[] list = Bukkit.getServer().getOnlinePlayers();
-		//Bukkit.broadcastMessage(ChatColor.RED + event.getFormat());
-		for(final Player p : list)
-		{
-			if(message.contains(p.getName()))
-			{
+		for(final Player p : list) {
+			if(message.contains(p.getName())) {
 				event.getRecipients().remove(p);
 				int nLength = p.getName().length();
 				int x = message.indexOf(p.getName());
@@ -37,19 +31,12 @@ public final class Listeners implements Listener
 				String b = message.substring(x, x+nLength);
 				String c = message.substring(x+nLength);				
 				String string2 = String.format(ChatColor.WHITE + "<" + event.getPlayer().getDisplayName() + ChatColor.WHITE + "> " + ChatColor.WHITE + a + ChatColor.GREEN + b + ChatColor.WHITE + c);
-				
-				/*String form = event.getFormat();
-				
-				String d = form.substring(0, x);
-				String e = form.substring(x, x+nLength);
-				String f = form.substring(x+nLength);*/
 						
 				p.sendMessage(string2);
-				//p.sendMessage(event.getFormat());
-				//message.replace(p.getName(), String.format(ChatColor.GREEN + p.getName()));
-				p.playSound(p.getLocation(), Sound.NOTE_PIANO, 2, (float) 0.5);      		
-        		Bukkit.getScheduler().scheduleSyncDelayedTask(chathighlight, new Runnable()
-        		{
+
+				p.playSound(p.getLocation(), Sound.NOTE_PIANO, 2, (float) 0.5);     
+				
+        		Bukkit.getScheduler().scheduleSyncDelayedTask(chathighlight, new Runnable() {
         			public void run()
         			{
         				p.playSound(p.getLocation(), Sound.NOTE_PIANO, 2, (float) .6);
